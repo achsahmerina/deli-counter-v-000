84e1e78b3551b1e131896a71cf6b380e41a31e2c
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 19:36:46 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,5 +19,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.length == 0
     puts "There is nobody waiting to be served!"
+  else
+    puts "First person in line is #{katz_deli.shift}"
   end
 end
