a595acf7684939b03cbade3fcb845290286b657f
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 19:28:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,4 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    line = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-      line += " #{index.to_i+1}.#{name}"
-  end
-  puts "#{line}"
-end
-end
+    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
