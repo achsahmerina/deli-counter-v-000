9552702a8c2c0232eeedcf470aa1cffe865edb8f
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 19:31:37 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,12 @@
-
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+    message="The line is currently:"
+
+    katz_deli.each_with_index do |value, index|
+      message += " #{index.to_i+1}. #{value}"
+    end
+
+    puts "#{message}"
   end
-end
