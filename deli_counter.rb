7ee3260a4853b6723fe72e4a204aa785971d31ec
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 19:30:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,5 @@ def line(another_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: 1. Amanda 2. Annette 3. Ruchi 4. Jason 5. Logan 6. Spencer 7. A
-vi 8. Joe 9. Rachel 10. Lindsey"
-end
+
 end
